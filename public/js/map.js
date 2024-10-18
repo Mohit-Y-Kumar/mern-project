53cfae82e4bb395ff0a7cb8712d4819c6ff5c246
Mohit Kumar 16 <my1362321@gmail.com>
2024-10-18 07:05:58 +0530

    Modify map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,16 @@
 
-maptoken = process.env.MAP_API_KEY;
 
+// Initialize the map and set the view to a desired location (Delhi in this example)
 var map = L.map('map').setView([28.6139, 77.2090], 13);
 
-L.tileLayer('https://maps.geoapify.com/v1/tile/osm-carto/{z}/{x}/{y}.png?apiKey=MAP_API_KEY', {
+// Add Geoapify tile layer to the map
+L.tileLayer('https://maps.geoapify.com/v1/tile/osm-carto/{z}/{x}/{y}.png?apiKey=mapapi', {
     attribution: '© OpenStreetMap contributors, © Geoapify',
     maxZoom: 20
 }).addTo(map);
 
+// Add a marker at the center location (Delhi in this case)
 var marker = L.marker([28.6139, 77.2090]).addTo(map);
 
-marker.bindPopup("<b>Delhi</b><br />India's capital city.").openPopup();
+// Optional: Add a popup to the marker
+marker.bindPopup("<b>Delhi</b><br>India's capital city.").openPopup();
